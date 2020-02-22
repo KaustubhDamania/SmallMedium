ed9b863fa760a12f3266d1b59a5e048143eeaab4
KaustubhDamania <kaustubh.damania@gmail.com>
2020-02-22 17:38:17 +0530

    Changed serviceworker href

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) { // show other serviceworker in application tabs
     try {
-        navigator.serviceWorker.register('../serviceworker.js');
+        navigator.serviceWorker.register('./serviceworker.js');
         console.log('SW registered');
     } catch (error) {
         console.log('SW failed');
