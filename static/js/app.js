7183eb98c2302680155faefe180b09f9dcdae7d6
KaustubhDamania <kaustubh.damania@gmail.com>
2020-02-24 00:03:59 +0530

    Removed comments

--- static/js/app.js
+++ static/js/app.js
@@ -10,55 +10,10 @@ window.onload = function() {
     }
 }
 
-// let prefixURL = 'https://smallmedium-postcontentfetch.herokuapp.com'
 let prefixURL = 'http://13.127.65.157:5001'
-
-// window.addEventListener('load', () => {
-//     const parsedUrl = new URL(window.location);
-//     // searchParams.get() will properly handle decoding the values.
-//     let title = parsedUrl.searchParams.get('text');
-//     if (title==null) {
-//         return;
-//     }
-//     console.log('Title shared: ' + title);
-//     title = title.split(' ');
-//     title = title[title.length-1];
-//     // let slashIndex = window.location.href.lastIndexOf('/')
-//     // let prefixURL = window.location.href.slice(0,slashIndex)
-//     // prefixURL = prefixURL.replace('5000','5001')
-//     let postURL = prefixURL + '/crawl.json?spider_name=get_content&url='
-//     postURL += encodeURIComponent(title)
-//     console.log('Final URL is', postURL);
-//     postRequest(postURL);
-// });
-
-function postRequest(url){
-    fetch(url, {
-        'method': 'GET'
-    })
-    .then(res => res.json())
-    .then(json => {
-        console.log(json);
-        // document.getElementsByTagName('head')[0].innerHTML += json.items[0].head
-        // document.getElementsByClassName('container')[0].innerHTML = json.items[0].body
-        // document.getElementsByClassName('container')[0].style = "margin-bottom: 50px;"
-
-    })
-    .catch(err => console.log(err));
-}
 
 let submit = document.getElementById('submit');
 function sendURL() {
-    // let slashIndex = window.location.href.lastIndexOf('/')
-    // let prefixURL = window.location.href.slice(0,slashIndex)
-    // prefixURL = prefixURL.replace('5000','5001')
-    // console.log(prefixURL);
-    // let postURL = prefixURL + '/crawl.json?spider_name=get_content&url='
-    // let articleURL = document.getElementById('url').value
-    // postURL += encodeURIComponent(articleURL)
-    // console.log('Final POST url',postURL);
-    // postRequest(postURL);
     let postURL = '?text=' + document.getElementById('url').value
     window.location.href = postURL
-    // postRequest(postURL);
 }
